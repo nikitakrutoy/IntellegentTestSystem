b93227122a3faa32507236a40482dc8dd6318606
nikitakrutoy <nikitakrutoy@gmail.com>
2017-03-31 20:02:31 +0300

    Delete setwebhook

--- setwebhook.py
+++
@@ -1,7 +0,0 @@
-import telepot
-import environ
-
-
-bot_token = '326195811:AAFLRPho2ptH6HkNdaJPKszQodGZb67o8yc'
-bot = telepot.Bot(bot_token)
-bot.setWebhook('https://glacial-hamlet-86113.herokuapp.com/its/bot/{bot_token}/'.format(bot_token=bot_token))
