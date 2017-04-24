8f71990ed74c672f4df7dd17e0a1e2ca20111663
NikitaKrutoy <nikitakrutoy@gmail.com>
2017-04-24 13:55:50 +0300

    testing inline buttons

--- its/views.py
+++ its/views.py
@@ -12,23 +12,6 @@ TelegramBot = telepot.Bot(settings.TELEGRAM_BOT_TOKEN)
 
 logger = logging.getLogger('telegram.bot')
 
-
-def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Press me', callback_data='press')],
-               ])
-
-    bot.sendMessage(chat_id, 'Use inline keyboard', reply_markup=keyboard)
-
-def on_callback_query(msg):
-    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-    print('Callback Query:', query_id, from_id, query_data)
-
-    bot.answerCallbackQuery(query_id, text='Got it')
-
-
 @csrf_exempt
 def reply(request, bot_token):
     if bot_token != settings.TELEGRAM_BOT_TOKEN:
@@ -38,6 +21,7 @@ def reply(request, bot_token):
     logger.info(raw)
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
                    [InlineKeyboardButton(text='Press me', callback_data='press')],
+                   [InlineKeyboardButton(text='Press me', url='https://yandex.ru/')],
                ])
 
     payload = json.loads(raw)
@@ -46,9 +30,8 @@ def reply(request, bot_token):
     #     if message["enteties"][0]["type"] == "bot_command":
     #         exec_command(message['text'])
     chat_id = payload['message']['chat']['id']
-    TelegramBot.sendMessage(chat_id, telepot.flavor(payload))
+    TelegramBot.sendMessage(chat_id, payload)
+    TelegramBot.sendMessage(chat_id, telepot.flavor(message))
     TelegramBot.sendMessage(chat_id, Question.objects.get(question_id = 0).text,
      reply_markup=keyboard)
-    bot.message_loop({'chat': on_chat_message,
-                  'callback_query': on_callback_query})
     return JsonResponse({}, status=200)
